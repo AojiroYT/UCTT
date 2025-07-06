7f443667b3b701622a6a1d46ca4bec73e38d31e4
AojiroYT <132192466+AojiroYT@users.noreply.github.com>
2025-07-06 17:32:35 +0100

    Update server.js

--- Uchesstactoe-online/server.js
+++ Uchesstactoe-online/server.js
@@ -10,28 +10,69 @@
 
   app.use(express.static('public'));
 
-  const games = {}; // { roomId: { moves: [], turn: 'white'|'black', sockets: {white: socketId, black: socketId} } }
+  const games = {}; // { roomId: { moves: [], turn: 'white'|'black', sockets: {white: socketId, black: socketId}, pass: string|null, public: bool } }
   const rooms = {}; // { roomId: [socketId1, socketId2] }
 
   io.on('connection', (socket) => {
     let currentRoom = null;
     let myColor = null;
 
+    socket.on('createRoom', ({ name, pass }) => {
+      if (rooms[name]) {
+        socket.emit('roomError', 'Room already exists!');
+        return;
+      }
+      rooms[name] = [socket.id];
+      games[name] = { moves: [], turn: 'white', sockets: {}, pass: pass || null, public: true };
+      currentRoom = name;
+      socket.join(name);
+      socket.emit('roomCreated', name);
+    });
+
+    socket.on('joinRoom', ({ name, pass }) => {
+      if (!rooms[name]) {
+        socket.emit('roomError', 'Room does not exist!');
+        return;
+      }
+      if (rooms[name].length >= 2) {
+        socket.emit('roomError', 'Room is full!');
+        return;
+      }
+      if (games[name].pass && games[name].pass !== pass) {
+        socket.emit('roomError', 'Incorrect password!');
+        return;
+      }
+      rooms[name].push(socket.id);
+      currentRoom = name;
+      socket.join(name);
+      socket.emit('roomJoined', name);
+      // Assign colors if two players
+      if (rooms[name].length === 2) {
+        games[name].sockets.white = rooms[name][0];
+        games[name].sockets.black = rooms[name][1];
+        io.to(rooms[name][0]).emit('assignColor', 'white');
+        io.to(rooms[name][1]).emit('assignColor', 'black');
+        io.to(name).emit('init', games[name].moves);
+      }
+    });
+
+    socket.on('listRooms', () => {
+      // List public rooms with <2 players
+      const publicRooms = Object.keys(games).filter(r => games[r].public && rooms[r] && rooms[r].length < 2).map(r => ({ name: r }));
+      socket.emit('publicRooms', publicRooms);
+    });
+
     socket.on('join', (roomId) => {
       socket.join(roomId);
       currentRoom = roomId;
       if (!rooms[roomId]) rooms[roomId] = [];
       if (!rooms[roomId].includes(socket.id)) rooms[roomId].push(socket.id);
-
-      // Wait for 2 players
       if (rooms[roomId].length === 2) {
-        // Assign colors
-        if (!games[roomId]) games[roomId] = { moves: [], turn: 'white', sockets: {} };
+        if (!games[roomId]) games[roomId] = { moves: [], turn: 'white', sockets: {}, public: true };
         games[roomId].sockets.white = rooms[roomId][0];
         games[roomId].sockets.black = rooms[roomId][1];
         io.to(rooms[roomId][0]).emit('assignColor', 'white');
         io.to(rooms[roomId][1]).emit('assignColor', 'black');
-        // Send current game state to both
         io.to(roomId).emit('init', games[roomId].moves);
       } else {
         socket.emit('waitingForOpponent');
@@ -41,14 +82,10 @@
     socket.on('move', (move) => {
       if (!currentRoom || !games[currentRoom]) return;
       const game = games[currentRoom];
-      // Only accept move from correct player
       const expectedSocket = game.sockets[game.turn];
       if (socket.id !== expectedSocket) return;
-      // Save move
       game.moves.push(move);
-      // Switch turn
       game.turn = (game.turn === 'white' ? 'black' : 'white');
-      // Broadcast move to both players, include nextTurn
       io.to(currentRoom).emit('move', { ...move, nextTurn: game.turn });
     });
 
